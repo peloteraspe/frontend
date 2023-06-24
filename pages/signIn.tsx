e3b389ad277715d81d6938f109411a38706f23b0
fiorellasaro <fiorellasaro27@gmail.com>
2023-06-24 18:02:42 -0500

    remove console

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -23,10 +23,6 @@ const SignIn: NextPage = () => {
   }, [isLoading, session]);
 
   const handleMagicLinkClick = async () => {
-    console.log(
-      process.env.NEXT_PUBLIC_URL,
-      process?.env?.NEXT_PUBLIC_VERCEL_URL
-    );
     try {
       setLoading(true);
       // const { error } = await supabase.auth.signIn({ email: form.getValues('email') });
@@ -34,7 +30,9 @@ const SignIn: NextPage = () => {
         email: form.getValues("email"),
         options: {
           emailRedirectTo: `${
-            process?.env?.NEXT_PUBLIC_VERCEL_URL ?? process.env.NEXT_PUBLIC_URL
+            process?.env?.NEXT_PUBLIC_VERCEL_URL !== undefined
+              ? process.env.NEXT_PUBLIC_VERCEL_URL
+              : process.env.NEXT_PUBLIC_URL
           }/dashboard`,
         },
       });
