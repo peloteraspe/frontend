0750c5efe8d7df7f9773249fbdf4744761c22d1e
Sofia <sofia.martinezsaenz@gmail.com>
2023-08-11 16:16:12 -0500

    update

--- pages/validate.tsx
+++ pages/validate.tsx
@@ -1,12 +1,11 @@
-import { NextPage } from 'next';
+import { NextPage } from "next";
 
 const Validate: NextPage = () => {
   return (
-    
     <div className="realtive flex flex-col justify-center h-screen text-center">
       <h1 className="mt-4 text-2xl font-semibold ">Revisa tu correo</h1>
       <p className="mt-10 text-lg px-20">
-       Te enviamos un correo para validar tu cuenta.
+        Te enviamos un correo para validar tu cuenta.
       </p>
     </div>
   );
