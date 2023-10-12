d45f6ffd7e59993e24abf66798498ecd60fd0795
fiorellasaro <fiorellasaro27@gmail.com>
2023-10-11 19:55:31 -0500

    fix problem in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import React from 'react';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-const CustomDocument = () => {
-  return (
-    <Html lang={'en'}>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={'anonymous'} />
-        <link
-          href='https://fonts.googleapis.com/css2?family=DM+Sans&family=Manrope:wght@400;500;600;700;800;900&&display=swap'
-          rel='stylesheet'
-        />
-        <meta
-          name='description'
-          content='Improve women’s soccer life by providing a platform for players to find teams and teams to find players.'
-        />
-        <link rel='icon' href='/favicon.ico' />
-        <title>Peloteras</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang={"en"}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin={"anonymous"}
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Manrope:wght@400;500;600;700;800;900&&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="Improve women’s soccer life by providing a platform for players to find teams and teams to find players."
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Peloteras</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
 export default CustomDocument;
