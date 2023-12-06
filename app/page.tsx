2c83005e5c3f9ff7a1b2795c8f34be03470389cb
andrealemonroy <andreale17@icloud.com>
2023-12-06 00:26:42 -0500

    add made by heart

--- app/page.tsx
+++ app/page.tsx
@@ -93,15 +93,10 @@ export default async function Index() {
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{' '}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
+          Hecho con{' '}
+          <span role="img" aria-label="Amor">
+            ❤️
+          </span>{' '}
         </p>
       </footer>
     </>
