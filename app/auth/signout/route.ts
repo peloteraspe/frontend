7fd0714f8aad1ee4f3d3908a560fedac6f00e62e
andrealemonroy <andreale17@icloud.com>
2024-01-27 01:49:58 -0500

    fix build

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,13 +1,15 @@
-// app/auth/signout.ts or app/auth/signout/route.ts
+import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
-import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
-export async function GET(request: { nextUrl: { clone: () => any } }) {
+
+export async function GET(request: NextRequest) {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
   await supabase.auth.signOut();
+
+  // Construct an absolute URL for the redirect
   const url = request.nextUrl.clone();
-  url.pathname = '/';
+  url.pathname = '/login';
 
   // Redirect to the login page after sign out
   return NextResponse.redirect(url);
