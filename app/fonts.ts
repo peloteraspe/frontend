83ebbbbe1d3dcda640c8523beaf3bc35b38a7df4
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-02-21 09:08:48 -0500

    Aplicar fuente Eastman a títulos desde TitleS hasta TitleXL

---
+++ app/fonts.ts
@@ -0,0 +1,10 @@
+import localFont from "next/font/local";
+
+export const eastmanBold = localFont({
+    src: "./assets/fonts/Eastman-Bold.ttf",
+    variable: "--font-eastman-bold",
+})
+export const eastmanExtrabold = localFont({
+    src: "./assets/fonts/Eastman-Extrabold.ttf",
+    variable: "--font-eastman-extrabold",
+})
