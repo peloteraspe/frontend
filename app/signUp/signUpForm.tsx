82208f6aef7a0662d854a43934815431b9b5eee8
andrealemonroy <andreale17@icloud.com>
2024-04-07 22:12:19 -0500

    fix signup

--- app/signUp/signUpForm.tsx
+++ app/signUp/signUpForm.tsx
@@ -1,17 +1,15 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import SelectComponent, { OptionSelect } from "@/components/SelectComponent";
-
-import { createClient } from "@/utils/supabase/client";
-import toast from "react-hot-toast";
-import { useRouter } from "next/navigation";
-import { ParagraphM, Title2XL } from "@/components/atoms/Typography";
-import Input from "@/components/Input";
-import { ButtonWrapper } from "@/components/Button";
-import { ProfileRequestBody } from "@/utils/interfaces";
-import { createProfile } from "../_actions/profile";
-import { levelFormatted, playerPositionsFormatted } from "@/utils/data";
+'use client';
+import React, { useEffect, useState } from 'react';
+import SelectComponent, { OptionSelect } from '@/components/SelectComponent';
+import { createClient } from '@/utils/supabase/client';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation'; // Corrected import
+import { ParagraphM, Title2XL } from '@/components/atoms/Typography';
+import Input from '@/components/Input';
+import { ButtonWrapper } from '@/components/Button';
+import { ProfileRequestBody } from '@/utils/interfaces';
+import { createProfile } from '../_actions/profile';
+import { levelFormatted, playerPositionsFormatted } from '@/utils/data';
 
 const UpdateProfile = ({ user }: any) => {
   const supabase = createClient();
@@ -19,49 +17,53 @@ const UpdateProfile = ({ user }: any) => {
   const [selectedMultiOptions, setSelectedMultiOptions] = useState<
     OptionSelect[]
   >([]);
-
-  const [buttonText, setButtonText] = useState("Crear cuenta");
-
+  const [buttonText, setButtonText] = useState('Crear cuenta');
   const [loading, setLoading] = useState(false);
   const [posiciones, setPosiciones] = useState<OptionSelect[]>([]);
   const [levelOption, setLevelOption] = useState<OptionSelect[]>([]);
   const [selectedLevelOptions, setSelectedLevelOptions] =
     useState<OptionSelect>();
-
-  const [usernameRes, setUsername] = useState("");
+  const [username, setUsername] = useState('');
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+    setLoading(true); // Start loading
+    console.log('username:', username);
     const reqProfile: ProfileRequestBody = {
-      user: user.id,
-      username: usernameRes,
+      user: user?.id, // Optional chaining for safety
+      username: username,
       level_id: selectedLevelOptions
         ? levelFormatted(selectedLevelOptions)
         : -1,
       player_position: playerPositionsFormatted(selectedMultiOptions),
     };
-    console.log(reqProfile);
 
-    // await createProfile(formData);
-  };
-
-  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-    console.log(e.target.value);
+    try {
+      console.log('reqProfile:', reqProfile);
+      await createProfile(reqProfile);
+      toast.success('Profile updated successfully');
+      router.push('/', { shallow: true });
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      toast.error('Failed to update profile');
+    } finally {
+      setLoading(false); // End loading
+    }
   };
 
   useEffect(() => {
-    // obtengo las posiciones y se las agrego a mi estado local para poder pasarselo al select multiple.
+    // Fetch player positions from the database and update state
     const fetchPosiciones = async () => {
       try {
         const { data, error } = await supabase
-          .from("player_position")
-          .select("id, name");
+          .from('player_position')
+          .select('id, name');
 
         if (error) {
-          console.error("Error fetching positions:", error);
+          console.error('Error fetching positions:', error);
+          // Consider adding user feedback here, e.g., using a toast notification
         } else {
-          const positionOptions = data.map((position: any) => ({
+          const positionOptions = data.map((position) => ({
             key: position.id,
             value: position.id,
             label: position.name,
@@ -69,17 +71,21 @@ const UpdateProfile = ({ user }: any) => {
           setPosiciones(positionOptions);
         }
       } catch (error) {
-        console.error("Error fetching positions:", error);
+        console.error('Error fetching positions:', error);
+        // Consider adding user feedback here, e.g., using a toast notification
       }
     };
+
+    // Fetch player levels from the database and update state
     const fetchLevels = async () => {
       try {
-        const { data, error } = await supabase.from("level").select("id, name");
+        const { data, error } = await supabase.from('level').select('id, name');
 
         if (error) {
-          console.error("Error fetching levels:", error);
+          console.error('Error fetching levels:', error);
+          // Consider adding user feedback here, e.g., using a toast notification
         } else {
-          const lvlOptions = data.map((level: any) => ({
+          const lvlOptions = data.map((level) => ({
             key: level.id,
             value: level.id,
             label: level.name,
@@ -87,19 +93,19 @@ const UpdateProfile = ({ user }: any) => {
           setLevelOption(lvlOptions);
         }
       } catch (error) {
-        console.error("Error fetching levels:", error);
+        console.error('Error fetching levels:', error);
+        // Consider adding user feedback here, e.g., using a toast notification
       }
     };
+
+    // Execute the fetch operations when the component mounts
     fetchPosiciones();
     fetchLevels();
-  }, []);
+  }, []); // The empty dependency array means this effect runs only once, after the initial render.
 
   return (
     <div className="flex flex-col justify-center items-center w-full gap-8 h-[calc(100vh-6rem)]">
-      <div className="flex flex-col items-center">
-        <Title2XL>Bienvenida a</Title2XL>
-        <Title2XL color="text-mulberry">Peloteras</Title2XL>
-      </div>
+      <div className="flex flex-col items-center">{/* UI elements */}</div>
       <div className="flex flex-col items-center">
         <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit}>
           <Input
@@ -108,14 +114,12 @@ const UpdateProfile = ({ user }: any) => {
             type="text"
             name="username"
             placeholderText="ejm: pelotera123"
-            value={usernameRes}
-            onChange={handleUsernameChange}
-            // onErrorChange={handleEmailErrorChange}
-            errorText="Debes ingresar un nombre "
-            // error={emailError}
-            // onChange={handleEmailChange}
+            value={username}
+            setFormValue={setUsername}
+            errorText="Debes ingresar un nombre"
             required
           />
+
           <label className="w-full">
             <div className="mb-1">
               <ParagraphM fontWeight="semibold">
@@ -123,7 +127,6 @@ const UpdateProfile = ({ user }: any) => {
                 <span className="text-red-500"> *</span>
               </ParagraphM>
             </div>
-
             <SelectComponent
               options={posiciones}
               onChange={(value: any) => setSelectedMultiOptions(value)}
@@ -139,15 +142,17 @@ const UpdateProfile = ({ user }: any) => {
                 <span className="text-red-500"> *</span>
               </ParagraphM>
             </div>
-
             <SelectComponent
               options={levelOption}
               onChange={(value: any) => setSelectedLevelOptions(value)}
               value={selectedLevelOptions}
             />
           </label>
+
           <div className="flex flex-col items-center w-80">
-            <ButtonWrapper width={"full"}>{buttonText}</ButtonWrapper>
+            <ButtonWrapper width={'full'} disabled={loading}>
+              {loading ? 'Actualizando...' : buttonText}
+            </ButtonWrapper>
           </div>
         </form>
       </div>
--- components/layout/navbar/UserMenu.tsx
+++ components/layout/navbar/UserMenu.tsx
@@ -7,12 +7,12 @@ import MenuItem from './MenuItem';
 import UserImage from './UserImage';
 
 const UserMenu = ({ user }: any) => {
-  const [isOpen, setIsOpen] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(false);
   const ref = useRef<HTMLDivElement>(null);
 
-  const toggleOpen = useCallback(()=>{
+  const toggleOpen = useCallback(() => {
     setIsOpen((prev) => !prev);
-  }, [])
+  }, []);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -25,72 +25,76 @@ const UserMenu = ({ user }: any) => {
     return () => window.removeEventListener('click', handleClickOutside);
   }, []);
 
-
   return (
     <div className="relative z-40" ref={ref}>
-        <div className="flex flex-row items-center gap-3">
-          <div className="flex flex-1 items-center justify-end">
-            <div className='flex items-center space-x-8'>
-              {user ? (
-                <>
-                  <Link
-                    href="#"
-                    className="font-poppins font-semibold text-sm text-mulberry"
-                  >
-                    Mis eventos
-                  </Link>
+      <div className="flex flex-row items-center gap-3">
+        <div className="flex flex-1 items-center justify-end">
+          <div className="flex items-center space-x-8">
+            {user ? (
+              <>
+                <Link
+                  href="#"
+                  className="font-poppins font-semibold text-sm text-mulberry"
+                >
+                  Mis eventos
+                </Link>
 
-                  <Link
-                    href="#"
-                    className="font-poppins font-semibold text-sm text-mulberry"
-                  >
-                    Mi perfil
-                  </Link>
+                <Link
+                  href="#"
+                  className="font-poppins font-semibold text-sm text-mulberry"
+                >
+                  Mi perfil
+                </Link>
 
-                  <div 
-                    onClick={toggleOpen}
-                    className="inline-flex items-center gap-2 justify-center text-xl rounded-full cursor-pointer transition"
-                  >
-                    <UserImage src={''}/>
-                    <ChevronDownIcon className="h-6 w-6 text-slate-400" aria-hidden="true" />
+                <div
+                  onClick={toggleOpen}
+                  className="inline-flex items-center gap-2 justify-center text-xl rounded-full cursor-pointer transition"
+                >
+                  <UserImage src={''} />
+                  <ChevronDownIcon
+                    className="h-6 w-6 text-slate-400"
+                    aria-hidden="true"
+                  />
+                </div>
+                {isOpen && (
+                  <div className="absolute right-0 z-20 w-56 py-2 mt-2 rounded-md shadow-md bg-white overflow-hidden  top-12 text-sm flex flex-col cursor-pointer">
+                    <Link href={'/'} className="mx-4 my-2 ">
+                      <p className="text-sm  font-semibold leading-none">
+                        {user.username}
+                      </p>
+                      <p className="text-sm leading-none text-muted-foreground">
+                        {user?.email}
+                      </p>
+                    </Link>
+                    <hr className="-mx-1 my-1 h-px bg-muted" />
+                    <MenuItem
+                      onClick={() => {
+                        toggleOpen();
+                        window.location.href = '/auth/signout';
+                      }}
+                    >
+                      Cerrar sesión
+                    </MenuItem>
                   </div>
-                  {isOpen && (
-                    <div className="absolute right-0 z-20 w-56 py-2 mt-2 rounded-md shadow-md bg-white overflow-hidden  top-12 text-sm flex flex-col cursor-pointer">
-                      <Link href={'/'} className="mx-4 my-2 ">
-                        <p className="text-sm  font-semibold leading-none">Peloteras</p>
-                        <p className="text-sm leading-none text-muted-foreground">
-                          peloteras@gmail.com
-                        </p>
-                      </Link>
-                      <hr className="-mx-1 my-1 h-px bg-muted"/>
-                      <MenuItem 
-                        onClick={()=>{
-                          toggleOpen();
-                          window.location.href = "/auth/signout";
-                        }}>
-                          Cerrar sesión
-                      </MenuItem>
-                    </div>
-                  )}
-                </>
-              ) : (
-                <>
-                  <Link href={'/login'} >
-                    <span className='font-poppins font-semibold text-sm text-mulberry'>Inicia sesión</span>
-                  </Link>
-                  <button
-                    className="px-4 py-[0.60rem] bg-btnBg-light hover:bg-btnBg-dark hover:shadow text-white rounded-xl my-0 mx-2 flex justify-center items-center relative box-border"
-                  >
-                    <ButtonM color='text-white'>Unirme a un evento</ButtonM>
-                  </button>
-                </>
-              )}
-            </div>
+                )}
+              </>
+            ) : (
+              <>
+                <Link href={'/login'}>
+                  <span className="font-poppins font-semibold text-sm text-mulberry">
+                    Inicia sesión
+                  </span>
+                </Link>
+                <button className="px-4 py-[0.60rem] bg-btnBg-light hover:bg-btnBg-dark hover:shadow text-white rounded-xl my-0 mx-2 flex justify-center items-center relative box-border">
+                  <ButtonM color="text-white">Unirme a un evento</ButtonM>
+                </button>
+              </>
+            )}
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserMenu
-
+export default UserMenu;
