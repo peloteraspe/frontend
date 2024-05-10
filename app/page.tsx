8b986c3064e0d0965b2ae00bb22637e3b0751f68
andrealemonroy <andreale17@icloud.com>
2024-05-09 21:58:46 -0500

    fix styles home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Header from "@/components/Header";
-import PostsList from "./posts-list";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
-import UpdateProfile from "./signUp/signUpForm";
-import Hero from "@/components/layout/hero";
-import CardEventList from "@/components/cardEvents/CardEventList";
-import Sidebar from "@/components/layout/sidebar/Sidebar";
-import { getFeatures } from "@/lib/data/getFeatures";
-import { getEvents } from "@/lib/data/getEvents";
+import Header from '@/components/Header';
+import PostsList from './posts-list';
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
+import UpdateProfile from './signUp/signUpForm';
+import Hero from '@/components/layout/hero';
+import CardEventList from '@/components/cardEvents/CardEventList';
+import Sidebar from '@/components/layout/sidebar/Sidebar';
+import { getFeatures } from '@/lib/data/getFeatures';
+import { getEvents } from '@/lib/data/getEvents';
 
 export default async function Index() {
   const cookieStore = cookies();
@@ -19,13 +19,13 @@ export default async function Index() {
   let userProfile = null;
   if (user) {
     const { data, error } = await supabase
-      .from("profile")
-      .select("*")
-      .eq("user", user.id)
+      .from('profile')
+      .select('*')
+      .eq('user', user.id)
       .single();
 
     if (error) {
-      console.error("Error fetching user profile:", error);
+      console.error('Error fetching user profile:', error);
     } else {
       userProfile = data;
     }
@@ -44,13 +44,17 @@ export default async function Index() {
       {/* <Header /> */}
       <Hero />
       <section className="md:max-w-screen-md lg:max-w-screen-md xl:max-w-screen-xl mx-auto px-4 sm:px-6 py-8 md:py-16 flex justify-between w-full">
-        <div className="md:flex md:justify-between" data-sticky-container>
-          <Sidebar features={features} events={events} />
-
+        <div
+          className="md:flex md:justify-between w-full"
+          data-sticky-container
+        >
+          {/* <Sidebar features={features} events={events} /> */}
+          
           {/* Main content */}
           <div className="w-full">
             <CardEventList />
           </div>
+          <div className="md:w-96"></div>
         </div>
       </section>
     </>
