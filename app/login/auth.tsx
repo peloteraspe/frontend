95131be7d61f81915c0c9d073be619f6b6ebfb1c
Fiorella Sánchez <fiorella@redi.run>
2024-05-14 23:46:01 -0500

    testing url redirect otp

--- app/login/auth.tsx
+++ app/login/auth.tsx
@@ -6,11 +6,8 @@ import { redirect } from "next/navigation";
 export const signIn = async (email: string) => {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const currentDomain =
-    headers().get("origin") === "http://localhost:3000"
-      ? "http://localhost:3000/auth/callback"
-      : "https://www.peloteras.com";
-
+  const currentDomain = headers().get("origin");
+  console.log("currentDomain", currentDomain);
   const { error } = await supabase.auth.signInWithOtp({
     email,
     options: {
