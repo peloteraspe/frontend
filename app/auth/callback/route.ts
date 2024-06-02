26ad2946bba5b67f2dc66673d2b11816a3c5ac02
andreamonroyc <xt8920@rimac.com.pe>
2024-06-01 20:01:46 -0500

    fix domain redirect

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,20 +1,25 @@
 import { createClient } from '@/utils/supabase/server';
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get('code');
+  // const requestUrl = new URL(request.url);
+  // const code = requestUrl.searchParams.get('code');
 
+  const currentDomain =
+    headers().get('origin') === 'http://localhost:3000'
+      ? 'http://localhost:3000/auth/callback'
+      : 'https://www.peloteras.com/auth/callback';
+
+  const code = new URL(request.url).searchParams.get('code');
   if (code) {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
     await supabase.auth.exchangeCodeForSession(code);
   }
-
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin);
+  return NextResponse.redirect(currentDomain);
 }
