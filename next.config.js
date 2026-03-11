54c210fcc4d113d9d57af0edb8fda3ca2832ca7c
Fiorella Sánchez <fiorellasaro27@gmail.com>
2026-03-10 23:08:23 -0500

    fix next build configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone"
+    turbopack: {
+        root: path.resolve(__dirname),
+    },
 }
 
 module.exports = nextConfig
